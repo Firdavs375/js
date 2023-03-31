f13a66c059d2c9b4a13ed97f8de70c2c5be5fadc
Firdavs375 <firdavss.3755@gmail.com>
2023-03-31 20:09:28 +0500

    flex

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+// 3
+
+
+function num(a, b) {
+    if (a == 6 || b == 6) {
+        console.log(true);
+    } else if (a + b == 6) {
+        console.log(true);
+    } else if (a - b == 6) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+
+num(3, 3)
+
+
+// 4
+
+
+const name = prompt("Ism kiriting")
+
+console.log("Salom " + name + " yaxshimisiz");
+
+
+// 5
+
+
+
